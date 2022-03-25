1357963912bffcaaa69cbb7ccf936c254e7f3ade
Chandra Lama <iamme24cl@gmail.com>
2022-03-25 14:28:26 -0700

    installed and ocnfigured tailswinds css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1 className="font-bold text-center text-red-500 mt-16">Is this working?</h1>
     </div>
   );
 }
