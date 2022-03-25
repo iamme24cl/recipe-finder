9d15553dd4ac6855b82cd76f945ed55861c681b9
chandra <iamme24cl@gmail.com>
2022-03-25 15:37:24 -0700

    installed and configured prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold text-center text-red-500 mt-16">Is this working?</h1>
+      <h1 className="font-bold text-center text-red-500 mt-16">
+        Is this working?
+      </h1>
       <div className="flex space-x-2 justify-center">
-        <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+        <button
+          type="button"
+          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Button
+        </button>
       </div>
     </div>
   );
