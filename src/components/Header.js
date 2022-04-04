aef2d4803e6563cd1e6af0b341d0a93f7de0ac77
Chandra Lama <iamme24cl@gmail.com>
2022-04-04 09:27:26 -0700

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,16 +21,32 @@ const Header = () => {
       </div>
       <input
         type="text"
+        className="
+        block
+        w-60
+        sm:w-96
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded 
+        transition
+        ease-in-out
+        m-0
+        focus:outline-none
+      "
         placeholder="Keywords"
-        className="border border-gray-300 rounded rounded-r-none py-1 px-3 focus:outline-none"
       />
-      <div className="flex space-x-2 justify-center">
+      <div className="flex space-x-2 justify-center ml-1 sm:ml-3">
         <button
           onClick={handleClick}
           type="button"
-          className="inline-block px-6 py-2.5 bg-red-500 text-white font-medium text-xs leading-tight uppercase rounded rounded-l-none shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
+          className="inline-block px-3 sm:px-6 py-2 bg-red-500 text-white font-normal sm:uppercase leading-tight rounded shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
         >
-          Search
+          search
         </button>
       </div>
     </header>
